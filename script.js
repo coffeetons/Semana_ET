20b9850050ff69ebaaf6bf2bf5f30ad94a9a7bb6
coffeetons <kenzomatheus10@gmail.com>
2025-09-30 23:16:18 -0300

    feat: added correctly the animation of the jumbo fading in and out

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
-const hideJumbotron = document.getElementById("jumbo");
-const hideIcon =document.getElementById("icon")
-    
-    function hideJumbo(){
-        hideJumbotron.classList.toggle("window-out")
-        // hideJumbotron.classList.remove("window")
-        hideIcon.classList.toggle("window-out")
-        // hideIcon.classList.remove("window")
-        
+console.log('hi'); // just a test
+
+
+function hideJumbo() {
+    // get current opacity value
+  const hideJumbotron = document.getElementById('jumbo');
+  const jumbotronStyle = window.getComputedStyle(hideJumbotron);
+  const opacityVal = parseFloat(jumbotronStyle.opacity);
+  console.log('the opacity is:', opacityVal);
+
+  if (opacityVal == 0) {
+    // fade in
+    // jumbo
+    hideJumbotron.classList.remove("opacity-0");
+    hideJumbotron.classList.add('opacity-100');
+
+  } else {
+    // fade out
+    hideJumbotron.classList.remove("opacity-100");
+    hideJumbotron.classList.add("opacity-0");
     }
+}
