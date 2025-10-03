6a7a733e491f17f0df9a4f67a0d721a43aea0193
coffeetons <kenzomatheus10@gmail.com>
2025-10-03 09:28:15 -0300

    feat: added a new feature in development, that adds a window in the page

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ function hideJumbo() {
   const jumbotronStyle = window.getComputedStyle(hideJumbotron);
   const opacityVal = parseFloat(jumbotronStyle.opacity);
   console.log('the opacity is:', opacityVal);
+  
 
   if (opacityVal == 0) {
     // fade in
@@ -20,3 +21,21 @@ function hideJumbo() {
     hideJumbotron.classList.add("opacity-0");
     }
 }
+const body = document.querySelector("body")
+const bodyStyle = window.getComputedStyle(body)
+const bodyWidth = parseInt
+
+// function app(){
+//   const main = document.getElementById("window")
+//   main.innerHTML = `
+//   <div id="window2" class="containerWindow d-flex align-items-center justify-content-center py-4">
+//       <div class="w-50 h-100 bg-light rounded-4 ">
+                    
+//       </div>
+//     </div>`
+// }
+
+function app(){
+  
+}
+
